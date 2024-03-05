8d204628d388acad30f88baf0b7ff5d429ccb22c
dmytrobaida <dmytrobaida@outlook.com>
2024-03-05 16:15:33 +0200

    updates lib

---
+++ cv.config.ts
@@ -0,0 +1,54 @@
+import { CvConfig } from '@portfolio.md/builder';
+
+export default {
+  meta: {
+    title: "Dmytro Baida's Portfloio",
+    description: 'Full-Stack Engineer',
+    verification: {
+      google: 'gVx2WIPNUmdGNPc-btJadTU-kkNt2Q2xnuFU5P9myZY',
+    },
+  },
+  home: {
+    title: 'Dmytro Baida',
+    url: '/',
+    logoName: 'logo',
+  },
+  pages: {
+    cv: {
+      file: 'file:///content/cv.md',
+      generate: {
+        pdf: true,
+        docx: true,
+      },
+      menu: {
+        title: 'Resume',
+      },
+    },
+    projects: {
+      file: 'file:///content/projects.md',
+      isMain: true,
+    },
+  },
+  socials: [
+    {
+      url: 'https://github.com/dmytrobaida',
+    },
+    {
+      url: 'https://www.linkedin.com/in/dmytrobaida',
+    },
+    {
+      url: 'https://leetcode.com/dmytrobaida',
+    },
+  ],
+  credentials: {
+    title: 'Dmytro Baida',
+    year: 2024,
+    url: '/',
+  },
+  images: {
+    logo: 'file:///content/logo.jpeg',
+  },
+  analytics: {
+    gTag: 'G-Z76X7VG7QN',
+  },
+} satisfies CvConfig;
